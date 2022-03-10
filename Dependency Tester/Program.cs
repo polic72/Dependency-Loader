97002b5f4b35d03354684547af976aaef368ccda
Garrett Stonis <GStonis@emduggan.com>
2022-03-10 10:11:19 -0500

    Just started adding the testing project.

---
+++ Dependency Tester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("A path to an assembly to test must be passed.");
+
+                return;
+            }
+        }
+    }
+}
